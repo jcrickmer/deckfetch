0ea8c53c7a81d4dd1552163ad5fdaf53825ffb30
Jason Crickmer <jcrickmer@gmail.com>
2015-05-04 18:42:37 -0500

    adding middleware component to reduce repeat visits. needs lots of testing.

---
+++ deckfetch/middlewares.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from scrapy import log
+from scrapy.exceptions import IgnoreRequest
+import MySQLdb as mdb
+
+class DedupMiddleware(object):
+
+    def __init__(self):
+        self.db_cur = None
+        try:
+            con = mdb.connect('localhost', 'testuser', 'test623', 'testdb');
+            self.db_cur = con.cursor()
+        except mdb.Error, e:
+            log.msg("Error connecting to database - %d: %s" % (e.args[0],e.args[1]), level=log.ERROR)
+
+    def process_request(self, request, spider):
+        if self.db_cur is not None:
+            db_res = self.db_cur.execute('SELECT id FROM deck WHERE url = %s', request.url)
+            if cur.fetchone():
+                log.msg('Ignore url that has already been fetched: <%s>'%url, level=log.DEBUG)
+                raise IgnoreRequest()
